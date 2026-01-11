6547bc9fb813a6dd0cc4580a481499e96d8f1bfd
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:42:00 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "logic_app_action_https" {
+  description = "All logic_app_action_http resources"
+  value       = azurerm_logic_app_action_http.logic_app_action_https
+}
+
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "logic_app_action_https" {
+  description = <<EOT
+Map of logic_app_action_https, attributes below
+Required:
+    - logic_app_id
+    - method
+    - name
+    - uri
+Optional:
+    - body
+    - headers
+    - queries
+    - run_after (block):
+        - action_name (required)
+        - action_result (required)
+EOT
+
+  type = map(object({
+    logic_app_id = string
+    method       = string
+    name         = string
+    uri          = string
+    body         = optional(string)
+    headers      = optional(map(string))
+    queries      = optional(map(string))
+    run_after = optional(object({
+      action_name   = string
+      action_result = string
+    }))
+  }))
+}
+
