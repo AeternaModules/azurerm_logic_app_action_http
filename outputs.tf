1d3746113905bc10bdb7621c6587106d9eed79b9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "logic_app_action_https" {
-  description = "All logic_app_action_http resources"
-  value       = azurerm_logic_app_action_http.logic_app_action_https
-}
 output "logic_app_action_https_body" {
-  description = "List of body values across all logic_app_action_https"
-  value       = [for k, v in azurerm_logic_app_action_http.logic_app_action_https : v.body]
+  description = "Map of body values across all logic_app_action_https, keyed the same as var.logic_app_action_https"
+  value       = { for k, v in azurerm_logic_app_action_http.logic_app_action_https : k => v.body }
 }
 output "logic_app_action_https_headers" {
-  description = "List of headers values across all logic_app_action_https"
-  value       = [for k, v in azurerm_logic_app_action_http.logic_app_action_https : v.headers]
+  description = "Map of headers values across all logic_app_action_https, keyed the same as var.logic_app_action_https"
+  value       = { for k, v in azurerm_logic_app_action_http.logic_app_action_https : k => v.headers }
 }
 output "logic_app_action_https_logic_app_id" {
-  description = "List of logic_app_id values across all logic_app_action_https"
-  value       = [for k, v in azurerm_logic_app_action_http.logic_app_action_https : v.logic_app_id]
+  description = "Map of logic_app_id values across all logic_app_action_https, keyed the same as var.logic_app_action_https"
+  value       = { for k, v in azurerm_logic_app_action_http.logic_app_action_https : k => v.logic_app_id }
 }
 output "logic_app_action_https_method" {
-  description = "List of method values across all logic_app_action_https"
-  value       = [for k, v in azurerm_logic_app_action_http.logic_app_action_https : v.method]
+  description = "Map of method values across all logic_app_action_https, keyed the same as var.logic_app_action_https"
+  value       = { for k, v in azurerm_logic_app_action_http.logic_app_action_https : k => v.method }
 }
 output "logic_app_action_https_name" {
-  description = "List of name values across all logic_app_action_https"
-  value       = [for k, v in azurerm_logic_app_action_http.logic_app_action_https : v.name]
+  description = "Map of name values across all logic_app_action_https, keyed the same as var.logic_app_action_https"
+  value       = { for k, v in azurerm_logic_app_action_http.logic_app_action_https : k => v.name }
 }
 output "logic_app_action_https_queries" {
-  description = "List of queries values across all logic_app_action_https"
-  value       = [for k, v in azurerm_logic_app_action_http.logic_app_action_https : v.queries]
+  description = "Map of queries values across all logic_app_action_https, keyed the same as var.logic_app_action_https"
+  value       = { for k, v in azurerm_logic_app_action_http.logic_app_action_https : k => v.queries }
 }
 output "logic_app_action_https_run_after" {
-  description = "List of run_after values across all logic_app_action_https"
-  value       = [for k, v in azurerm_logic_app_action_http.logic_app_action_https : v.run_after]
+  description = "Map of run_after values across all logic_app_action_https, keyed the same as var.logic_app_action_https"
+  value       = { for k, v in azurerm_logic_app_action_http.logic_app_action_https : k => v.run_after }
 }
 output "logic_app_action_https_uri" {
-  description = "List of uri values across all logic_app_action_https"
-  value       = [for k, v in azurerm_logic_app_action_http.logic_app_action_https : v.uri]
+  description = "Map of uri values across all logic_app_action_https, keyed the same as var.logic_app_action_https"
+  value       = { for k, v in azurerm_logic_app_action_http.logic_app_action_https : k => v.uri }
 }
 
